60e7c62b535264b7fa629ce5ff52b50bd6729bac
Earlopain <earlopain@c5h8no4na.net>
2019-05-07 02:50:24 +0200

    Added network, xml functions

--- util.js
+++ util.js
@@ -1,3 +1,8 @@
+const request = require("request");
+const fs = require("fs");
+const crypto = require('crypto');
+const xmlParserFunction = require('xml2js').parseString;
+
 function valueCheck(that) {
     for (const key of Object.keys(that)) {
         if (key === "thumbpath" && that[key] === undefined)
@@ -7,4 +12,39 @@ function valueCheck(that) {
     }
 }
 
+function getJSON(url, headers) {
+    return new Promise(resolve => {
+        request({
+            method: "GET", uri: encodeURI(url), headers: headers     //enocdeURI takes care of chars like é which
+        }, (error, response, body) => {
+            if (error)
+                debugger;
+            resolve(JSON.parse(body));
+        });
+    });
+}
+
+function postJSON(url, headers2, data) {
+    return new Promise(resolve => {
+        request({
+            method: "POST", uri: encodeURI(url), headers: headers2, formData: data     //enocdeURI takes care of chars like é which
+        }, (error, response, body) => {
+            if (error)
+                debugger;
+            resolve(JSON.parse(body));
+        });
+    });
+}
+
+function xmlParser(xmlstring) {
+    return new Promise(resolve => {
+        xmlParserFunction(xmlstring, function (err, result) {
+            resolve(result);
+        });
+    })
+}
+
 exports.valueCheck = valueCheck;
+exports.getJSON = getJSON;
+exports.postJSON = postJSON;
+exports.xmlParser = xmlParser;
