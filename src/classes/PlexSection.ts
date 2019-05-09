09bd4a5a400ae3c4fc730d06c76259d44801fb3d
Earlopain <earlopain@c5h8no4na.net>
2019-05-09 20:41:31 +0200

    Added getAllContent, construct using only needed json instead of full response

--- src/classes/PlexSection.ts
+++ src/classes/PlexSection.ts
@@ -2,32 +2,31 @@ import { PlexPicture } from "./PlexPicture.js";
 import { PlexArtist } from "./PlexArtist.js";
 import util = require("../util.js");
 import { PlexServer } from "./PlexServer.js";
+import { PlexSong } from "./PlexSong.js";
 export class PlexSection {
 
     title: string;
     id: number;
-    size: number;
     type: string;
     files: (PlexArtist | PlexPicture)[];
-    server: PlexServer
+    server: PlexServer;
+    localLocations: string[];
 
     constructor(json, server: PlexServer) {
-        this.title = json.MediaContainer.librarySectionTitle;
-        this.id = json.MediaContainer.librarySectionID;
-        this.size = json.MediaContainer.size;
-        this.type = json.MediaContainer.viewGroup;
+        this.title = json.title;
+        this.id = json.key;
+        this.type = json.type;
         this.files = [];
         this.server = server;
+        this.localLocations = json.Location.map(element => {return element.path});
         util.valueCheck(this);
     }
 
-    async getContent(): Promise<(PlexArtist | PlexPicture)[]> {
+    async getAllContent(): Promise<(PlexArtist | PlexPicture)[]> {
         if (this.files.length !== 0)
             return this.files;
         const json = await this.server.request("/library/sections/" + this.id + "/all", "GET");
-
-        if (this.files.length !== 0)
-            return this.files;
+        
         switch (this.type) {
             case "photo":
                 for (const file of json.MediaContainer.Metadata) {
@@ -43,5 +42,29 @@ export class PlexSection {
                 break;
         }
         return this.files;
+    }
+
+    async getAllFiles(): Promise<(PlexPicture | PlexSong)[]> {
+        let result = [];
+        let json;
+        switch (this.type) {
+            case "artist":
+                json = await this.server.request("/library/sections/" + this.id + "/all?type=10");
+                for (const song of json.MediaContainer.Metadata) {
+                    result.push(new PlexSong(song, this.id, this.server));
+                }
+                break;
+            case "photo":
+                json = await this.server.request("/library/sections/" + this.id + "/all?type=12,13");
+                for (const picture of json.MediaContainer.Metadata) {
+                    result.push(new PlexPicture(picture, this.id, this.server));
+                }
+                break;
+
+            default:
+                debugger;
+                break;
+        }
+        return result;
     }
 }
